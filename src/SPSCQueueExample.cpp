410acdaada9625a10a6cd2f0a48e5cdf1fc19fdd
Villou Z <513585604@qq.com>
2026-01-28 15:49:43 +0800

    学习完毕

--- src/SPSCQueueExample.cpp
+++ src/SPSCQueueExample.cpp
@@ -7,19 +7,29 @@ int main(int argc, char *argv[]) {
 
   using namespace rigtorp;
 
-  SPSCQueue<int> q(1);
-
-  std::cout << "sizeof(SPSCQueue) = " << sizeof(SPSCQueue<int>) << "\n";   // ¡Ý 64 + padding
-
+  SPSCQueue<int> q(1001);
 
   auto t = std::thread([&] {
-    while (!q.front())
-      ;
-    std::cout << *q.front() << std::endl;
-    q.pop();
+      while(true)
+      {
+          if (q.front())
+          {
+              std::cout << *q.front() << std::endl;
+              q.pop();
+          }
+      }
   });
-  q.push(1);
+
+  auto t1 = std::thread([&]
+  {
+      for (int i = 0; i < 200000; ++i)
+      {
+          q.push(i);
+      }
+  });
+
   t.join();
+  t1.join();
 
   return 0;
 }
